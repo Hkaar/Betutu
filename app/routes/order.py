f67a185f5a1b1bda722ad142e4b1673eecf7bfdf
Hkaar <HkaarDev@gmail.com>
2024-02-21 09:46:33 +0800

    Added goto order route in orders

--- app/routes/order.py
+++ app/routes/order.py
@@ -1,10 +1,28 @@
 from fastapi import APIRouter, Depends, Request, status, Form
+from fastapi.responses import FileResponse, RedirectResponse
 
 from app.controllers.order import OrderController
 
 from app.dependecies.auth import SessionAuth
 
 orderRouter = APIRouter(prefix="/order")
+
+@orderRouter.get("/")
+async def goto_order(request: Request):
+    token = request.cookies.get("orderToken", None)
+
+    if token:
+        return FileResponse("public/views/menu.html")
+    return RedirectResponse(url="/order/new")
+
+@orderRouter.get("/new")
+async def new_order(request: Request):
+    token = await OrderController.create(request)
+
+    response = RedirectResponse(url="/order")
+    response.set_cookie("orderToken", token)
+
+    return response
 
 @orderRouter.get("/all/{type}")
 async def get_all_orders(request: Request, type: str = None):
